b022eec7ed74a055183278e31f62d84bc9f77bdd
aryelzx <arielcearamor102@gmail.com>
2023-07-12 21:21:34 -0300

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const myArray = [
+  "teste 1",
+  "teste 2",
+  "teste 3",
+  "teste 4",
+  // arrayThree = [
+  //   "um",
+  //   "dois",
+  //   "tres"
+  // ]
+]
+const arrayTwo = [
+  "primeiro",
+  "segundo",
+  "terceiro",
+  quarto=[{
+    "objeto": "quarto"
+  },
+    "quarto",
+    "quinto",
+  ]
+]
+//PUSH
+myArray.push(arrayTwo);
+//unshift
+// const arrayField = myArray.unshift(arrayTwo)
+// const dadoArray = arrayField
+console.log(myArray[4][3][0])
